61663c9c7592d037c3a899ee537d752af09a68e4
Victor Moraes <vhugo006@gmail.com>
2023-02-08 13:40:14 -0300

    Add a function that removes even integers from an array

---
+++ arrays/removing-even-integers-from-an-array.js
@@ -0,0 +1,10 @@
+function removeEvenIntegers(arr){
+    return arr.filter( value => (value % 2) > 0);
+}
+
+const oddIntegers = removeEvenIntegers([0,1,2,5]);
+const expectedIntegers = [1,5];
+
+evenIntegersRemovedSuccessfully = oddIntegers.toString() === expectedIntegers.toString();
+
+console.log("Even Integers Removed Successfully: " + evenIntegersRemovedSuccessfully);
